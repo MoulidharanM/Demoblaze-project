7d9155b08871fe52a20ae243953d030d482f0563
MoulidharanM <dharansow2017@gmail.com>
2026-01-21 20:58:18 +0530

    html

--- playwright.config.js
+++ playwright.config.js
@@ -19,17 +19,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['playwright-smart-reporter', { 
-      outputFile: 'playwright-report/index.html', // Path where the report will be saved
-      historyFile: 'test-history.json',          // Optional: for tracking flakiness
-      maxHistoryRuns: 10,
-    }],
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
