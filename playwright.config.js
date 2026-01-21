12d17f52ac09cbf3e8ed7b9d7f461b972eed1f50
MoulidharanM <dharansow2017@gmail.com>
2026-01-21 20:17:09 +0530

    smart reporter

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['playwright-smart-reporter', { 
+      outputFile: 'playwright-report/index.html', // Path where the report will be saved
+      historyFile: 'test-history.json',          // Optional: for tracking flakiness
+      maxHistoryRuns: 10,
+    }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
